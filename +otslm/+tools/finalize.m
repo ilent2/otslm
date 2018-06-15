3aad55cc6cf33220499a1ff2de19df0c1597ee95
Isaac Lenton <isaac@isuniversal.com>
2018-06-15 18:29:37 +1000

    Simplified finalize

--- +otslm/+tools/finalize.m
+++ +otslm/+tools/finalize.m
@@ -1,4 +1,4 @@
-function pattern = finalize(input, varargin)
+function pattern = finalize(pattern, varargin)
 % FINALIZE finalize a pattern, applying a color map and taking the modulo.
 %
 %   pattern = finalize(input, varargin) finalizes the pattern.
@@ -7,16 +7,30 @@ function pattern = finalize(input, varargin)
 %
 %   'modulo'    mod     Applies modulo to the pattern, default 1.0.
 %   'colormap'  lookup  Applies the nearest value colour map lookup.
+%       May also be:
+%         'pmpi' for -pi to pi range.
+%         '2pi' for 0 to 2*pi range.
 
 p = inputParser;
 p.addParameter('modulo', 1.0);
-p.addParameter('colormap', []);
+p.addParameter('colormap', 'pmpi');
 p.parse(varargin{:});
 
 if ~isempty(p.Results.modulo)
-  input = mod(input, p.Results.modulo);
+  pattern = mod(pattern, p.Results.modulo);
 end
 
-% TODO: Apply colour map
-
-pattern = input;
+% Apply colour map
+if ischar(p.Results.colormap)
+  switch p.Results.colormap
+    case 'pmpi'
+      pattern = pattern*2*pi - pi;
+    case '2pi'
+      pattern = pattern*2*pi;
+    otherwise
+      error('Unrecognized colormap string');
+  end
+else
+  % TODO: Lookup tables
+  error('Other colourmaps not yet implemented');
+end
