c6c040174b109862f2bc7892860a95d01e48fe7f
Isaac Lenton <isaac@isuniversal.com>
2018-06-15 18:29:18 +1000

    Made combine more useful

--- +otslm/+tools/combine.m
+++ +otslm/+tools/combine.m
@@ -3,27 +3,41 @@ function pattern = combine(inputs, varargin)
 %
 % pattern = combine(inputs, ...) combines the cell array of patterns.
 %
+% Typical input should be a pattern between 0 and 1.
+% Most methods output range is between 0 and 1.
+%
+% See also Di Leonardo, Ianni and Ruocco (2007).
+%
 % Optional named parameters:
 %
 %   'method'    method    Method to use when combinging patterns.
-%       Supported methods:
-%         expangle      Uses angle(\sum_ii exp(1i*2*pi*inputs(ii)))
+%
+%       Methods to create multiple beams:
+%         dither        Randomly chooses values from different patterns
+%         super         Uses phi = angle(\sum_ii exp(1i*2*pi*inputs(ii)))
+%         rsuper        Superposition with random offset for each layer
+%         gs            Applies GS algorithm to try and generate the
+%             pattern that would be created by the sum of the individual
+%             beams.  Starts with an initial guess using rsuper.
+%
+%       Methods to modulate a beam pattern:
 %         add           Adds the patterns: \sum_ii inputs(ii)
 %         multiply      Multiplies the patterns: \prod_ii inputs(ii)
-%         dither        Randomly chooses values from different patterns
-%       Default method: expangle.
+%         addangle      Uses phi = angle(\prod_ii exp(1i*2*pi*inputs(ii)))
+%
+%       Default method: super.
 %
 %   'weights'   [weights] Array of weights for each pattern.
 %       If the method is dither, this is the percentage of each pattern
 %       to include in the result.  Not yet used by other methods.
 
 p = inputParser;
-p.addParameter('method', 'expangle');
+p.addParameter('method', 'super');
 p.addParameter('weights', []);
 p.parse(varargin{:});
 
 switch p.Results.method
-  case 'expangle'
+  case 'super'
 
     pattern = zeros(size(inputs{1}));
 
@@ -31,7 +45,43 @@ switch p.Results.method
       pattern = pattern + exp(1i*2*pi*inputs{ii});
     end
 
-    pattern = angle(pattern);
+    pattern = (angle(pattern)/pi+1)/2;
+
+  case 'rsuper'
+
+    offsets = rand([1, length(inputs)])*2*pi;
+
+    pattern = zeros(size(inputs{1}));
+
+    for ii = 1:length(inputs)
+      pattern = pattern + exp(1i*2*pi*inputs{ii} + 1i*offsets(ii));
+    end
+
+    pattern = (angle(pattern)/pi+1)/2;
+
+  case 'gs'
+
+    % Calculate initial guess using random phase superposition
+    guess = otslm.tools.combine(inputs, varargin{:}, ...
+        'method', 'rsuper');
+
+    incident = ones(size(inputs{1}));
+
+    % Calculate the target using fft
+    target = zeros(size(inputs{1}));
+    for ii = 1:length(inputs)
+      padding = 0;
+      vis = otslm.tools.visualise(2*pi*inputs{ii}, ...
+          'incident', incident, 'method', 'fft', 'padding', padding);
+      target = target + abs(vis(padding+1:end-padding, padding+1:end-padding));
+    end
+    target = target ./ length(inputs);
+
+    % Calculate the pattern with GS algorithm
+    pattern = otslm.iter.gs(target, 'guess', guess, ...
+        'incident', incident);
+
+    pattern = (pattern/pi+1)/2;
 
   case 'add'
 
@@ -48,6 +98,15 @@ switch p.Results.method
     for ii = 1:length(inputs)
       pattern = pattern .* inputs{ii};
     end
+
+  case 'addangle'
+
+    pattern = ones(size(inputs{1}));
+    for ii = 1:length(inputs)
+      pattern = pattern .* exp(1i*2*pi*inputs{ii});
+    end
+
+    pattern = (angle(pattern)/pi+1)/2;
 
   case 'dither'
 
