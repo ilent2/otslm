6a67f2a54fb5277db3c09e89343a81ad8c1192e5
Isaac Lenton <isaac@isuniversal.com>
2018-06-17 21:00:51 +1000

    Tweaks to spherical to make it easier to use

--- +otslm/+simple/spherical.m
+++ +otslm/+simple/spherical.m
@@ -1,6 +1,9 @@
 function pattern = spherical(sz, radius, varargin)
 % SPHERICAL generates a spherical lens pattern
 %
+% pattern = spherical(sz, radius, ...) generates a spherical pattern
+% with values from 0 (at the edge) and 1*sign(radius) (at the centre).
+%
 % pattern = spherical(sz, radius, ...) generates a spherical lens with
 % specified radius.  Default location is centre of image.  Values
 % outside the spehre are replaced with +/- Inf depending on sign of
@@ -8,5 +11,18 @@ function pattern = spherical(sz, radius, varargin)
 %
 % See simple.aspheric for more information and named arguments.
 
-pattern = otslm.simple.aspheric(sz, radius, 0, varargin{:});
+p = inputParser;
+p.KeepUnmatched = true;
+p.addParameter('imag_value', 0.0);
+p.parse(varargin{:});
+
+pattern = otslm.simple.aspheric(sz, radius, 0, varargin{:}, ...
+    'imag_value', NaN);
+
+% Scale the result
+pattern = pattern ./ max(abs(pattern(:))) - sign(radius);
+
+% Ensure result is real
+imag_parts = isnan(pattern);
+pattern(imag_parts) = p.Results.imag_value;
 
