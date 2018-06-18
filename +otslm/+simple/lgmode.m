834c71abbcc48bc177966cb2ca3bd5878bbbcd6f
Isaac Lenton <isaac@isuniversal.com>
2018-06-18 15:01:11 +1000

    Added amplitude output to simple.lgmode

--- +otslm/+simple/lgmode.m
+++ +otslm/+simple/lgmode.m
@@ -1,93 +1,64 @@
-function pattern = lgmode(sz, amode, rmode, varargin)
+function [phase, amplitude] = lgmode(sz, amode, rmode, varargin)
 % LGMODE generates the phase pattern for a LG beam
 %
 % pattern = lgbeam(sz, amode, rmode, radius, ...) generates the phase
 % pattern with azimuthal order amode, radial order rmode.
 %
+% [phase, amplitude] = lgbeam(...) also generates the amplitude pattern.
+%
 % Optional named parameters:
 %
 %   'centre'    [ x, y ]    centre location (default: pattern centre)
+%   'aspect'    aspect      aspect ratio of generated pattern
+%   'angle'     angle       Rotation angle about axis (radians)
+%   'angle_deg' angle       Rotation angle about axis (degrees)
 %   'radius'    radius      scaling factor for radial mode rings
+%   'p0'        p0          incident amplitude correction factor
+%       Should be 1.0 (default) for plane wave illumination (w_i = Inf),
+%       for Gaussian beams should be p0 = 1 - radius^2/w_i^2.
 
+% Check mode numbers
 assert(rmode >= 0, 'Radial mode must be >= 0');
 assert(floor(rmode) == rmode, 'Radial mode must be integer');
 assert(floor(amode) == amode, 'Azimuthal mode must be integer');
 
+% Parse inputs
 p = inputParser;
-p.addParameter('centre', [sz(2)/2, sz(1)/2]);
-p.addParameter('radius', min(sz(1), sz(2))/2 ...
-    / sqrt(max(1, rootBound(amode, rmode))));
+p.addParameter('centre', [ sz(2)/2, sz(1)/2 ]);
+p.addParameter('aspect', 1.0);
+p.addParameter('angle', []);
+p.addParameter('angle_deg', []);
+p.addParameter('radius', min(sz(1), sz(2))/10);
+p.addParameter('p0', 1.0);
 p.parse(varargin{:});
 
-% Generate grid
-[xx, yy] = meshgrid(1:sz(2), 1:sz(1));
-
-% Move centre of pattern
-xx = xx - p.Results.centre(1);
-yy = yy - p.Results.centre(2);
-
-% Calculate circular coordinates
-rho = sqrt(xx.^2 + yy.^2);
-phi = atan2(yy, xx);
+% Generate coordinates
+[~, ~, rho, phi] = otslm.simple.grid(sz, ...
+    'centre', p.Results.centre, 'aspect', p.Results.aspect, ...
+    'angle', p.Results.angle, 'angle_deg', p.Results.angle_deg);
 
 % Calculate azimuthal part of pattern
-pattern = amode .* phi ./ (2.0*pi);
+phase = amode .* phi ./ (2.0*pi);
 
-% Calculate radial part of pattern
+% Calculate laguerre polynomials in radial direction
+maxrho = max(rho(:));
+rr = linspace(0.0, maxrho, 2*maxrho);
+Lpoly_rr = laguerreL(rmode, abs(amode), (rr./p.Results.radius).^2);
 
-roots = findLgRoots(amode, rmode) * p.Results.radius;
-for ii = 1:length(roots)
-  pattern(rho <= roots(ii)) = pattern(rho <= roots(ii)) + 0.5;
-end
+% Interpolate between points using splines
+Lpoly_rho = interp1((rr./p.Results.radius).^2, Lpoly_rr, ...
+    (rho./p.Results.radius).^2, 'spline');
 
-% This is too slow
-%lg = laguerreL(rmode, abs(amode), rho ./ p.Results.radius);
-%pattern = pattern + (sign(lg) > 0)*0.5;
+% Calculate radial part of phase
+phase = phase + (sign(Lpoly_rho) > 0)*0.5;
 
-end
+% Calculate the amplitude too
+if nargout == 2
 
-function value = rootBound(amode, rmode)
-% Calculate the upper bound of the root (Wikipedia formula)
-
-  value = rmode + abs(amode) + (rmode - 1)*sqrt(rmode+abs(amode));
-
-end
-
-function roots = findLgRoots(amode, rmode)
-% Find the roots of the lg polynomial
-
-if rmode == 0
-  roots = [];
-elseif rmode == 1
-  roots = [ 1 + abs(amode) ];
-else
-
-  x = linspace(0, 1+rootBound(amode, rmode), 50*rmode);
-  lg = laguerreL(rmode, abs(amode), x);
-
-  start = 1;
-
-  roots = [];
-  while length(roots) < rmode
-
-    first_positive = find(sign(lg(start:end)) >= 0, 1);
-    first_negative = find(sign(lg(start:end)) < 0, 1);
-
-    if first_positive < first_negative
-      roots(end+1) = fzero(@(v) laguerreL(rmode, abs(amode), v), ...
-          x(start + first_negative - 1));
-      start = start + first_negative;
-    else
-      roots(end+1) = fzero(@(v) laguerreL(rmode, abs(amode), v), ...
-          x(start + first_positive - 1));
-      start = start + first_positive;
-    end
-
-  end
+  amplitude = (rho./p.Results.radius).^abs(2*amode) ...
+      .* Lpoly_rho .* exp(-p.Results.p0*0.5*(rho./p.Results.radius).^2);
 
 end
-
-roots = sqrt(roots./2);
 
 end
 
