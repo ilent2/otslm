14e85cd0a62381ab4ce36a480456f9f52b9a7a36
Isaac Lenton <isaac@isuniversal.com>
2018-06-25 18:56:39 +1000

    hgmode now returns amplitude too

--- +otslm/+simple/hgmode.m
+++ +otslm/+simple/hgmode.m
@@ -1,8 +1,11 @@
-function pattern = hgmode(sz, xmode, ymode, varargin)
+function [pattern, amplitude] = hgmode(sz, xmode, ymode, varargin)
 % HGMODE generates the phase pattern for a HG beam
 %
-% pattern = lgbeam(sz, amode, rmode, radius, ...) generates the phase
-% pattern with azimuthal order amode, radial order rmode.
+% pattern = hgmode(sz, xmode, ymode, ...) generates the phase
+% pattern with x and y mode numbers.
+%
+% [phase, amplitude] = hgmode(...) also calculates the signed
+% amplitude of the pattern in addition to the phase.
 %
 % Optional named parameters:
 %
@@ -19,73 +22,49 @@ assert(floor(ymode) == ymode, 'ymode must be integer');
 
 p = inputParser;
 p.addParameter('centre', [sz(2)/2, sz(1)/2]);
-p.addParameter('scale', 2*max(xmode, ymode)/sqrt(sz(1)^2 +sz(2)^2));
 p.addParameter('aspect', 1.0);
 p.addParameter('angle', []);
 p.addParameter('angle_deg', []);
+p.addParameter('scale', sqrt(sz(1)^2 +sz(2)^2)/(2*max(xmode, ymode)));
 p.parse(varargin{:});
 
-% Generate grid
-[xx, yy] = meshgrid(1:sz(2), 1:sz(1));
+% Generate coordinates
+[xx, yy] = otslm.simple.grid(sz, ...
+    'centre', p.Results.centre, 'aspect', p.Results.aspect, ...
+    'angle', p.Results.angle, 'angle_deg', p.Results.angle_deg);
 
-% Move centre of pattern
-xx = xx - p.Results.centre(1);
-yy = yy - p.Results.centre(2);
-
-% Calculate x and y directions
-
-angle = [];
-if ~isempty(p.Results.angle)
-  assert(isempty(angle), 'Angle set multiple times');
-  angle = p.Results.angle;
-end
-if ~isempty(p.Results.angle_deg)
-  assert(isempty(angle), 'Angle set multiple times');
-  angle = p.Results.angle_deg * pi/180.0;
-end
-if isempty(angle)
-  angle = 0.0;
-end
-
-dx = [cos(angle), -sin(angle)];
-dy = [sin(angle),  cos(angle)];
-
-% Apply aspect ratio
-dy = dy * p.Results.aspect;
-
-% Apply scaling
-dx = dx * p.Results.scale;
-dy = dy * p.Results.scale;
+% Apply scaling to the coordinates
+xx = xx ./ p.Results.scale;
+yy = yy ./ p.Results.scale;
 
 % Calculate pattern
 
-% Too slow
-%hx = hermiteH(xmode, xx .* dx(1) + yy .* dx(2));
-%hy = hermiteH(ymode, xx .* dy(1) + yy .* dy(2));
-%pattern = mod(0.5*(sign(hx)>0) + 0.5*(sign(hy)>0), 1)*0.5;
-
-xm = xx .* dx(1) + yy .* dx(2);
-ym = xx .* dy(1) + yy .* dy(2);
-
-xr = linspace(min(xm(:)), max(xm(:)), ceil(sqrt(sz(2)^2 + sz(1)^2)));
-yr = linspace(min(ym(:)), max(ym(:)), ceil(sqrt(sz(2)^2 + sz(1)^2)));
+xr = linspace(min(xx(:)), max(xx(:)), ceil(sqrt(sz(2)^2 + sz(1)^2)));
+yr = linspace(min(yy(:)), max(yy(:)), ceil(sqrt(sz(2)^2 + sz(1)^2)));
 
 hx = hermiteH(xmode, xr);
 hy = hermiteH(ymode, yr);
 
-pattern = zeros(sz);
+pattern = ones(sz);
 
 for ii = 2:length(xr)
-  idx = xm >= xr(ii-1) & xm < xr(ii);
-  pattern(idx) = pattern(idx) + 0.5*(hx(ii-1)>0);
+  idx = xx >= xr(ii-1) & xx < xr(ii);
+  pattern(idx) = pattern(idx) .* hx(ii-1);
 end
-idx = xm >= xr(end);
-pattern(idx) = pattern(idx) + 0.5*(hx(end)>0);
+idx = xx >= xr(end);
+pattern(idx) = pattern(idx) .* hx(end);
 
 for ii = 2:length(yr)
-  idx = ym >= yr(ii-1) & ym < yr(ii);
-  pattern(idx) = pattern(idx) + 0.5*(hy(ii-1)>0);
+  idx = yy >= yr(ii-1) & yy < yr(ii);
+  pattern(idx) = pattern(idx) .* hy(ii-1);
 end
-idx = ym >= yr(end);
-pattern(idx) = pattern(idx) + 0.5*(hy(end)>0);
+idx = yy >= yr(end);
+pattern(idx) = pattern(idx) .* hy(end);
+
+% Normalize amplitude maximum value
+amplitude = pattern ./ max(abs(pattern(:)));
+
+% Generate phase pattern
+pattern = (pattern >= 0.0) * 0.5;
+
 
