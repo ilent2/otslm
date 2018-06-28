9ee443a325f8c2b5c1e7a7dd3d61aa330de42126
Isaac Lenton <isaac@isuniversal.com>
2018-06-28 18:48:54 +1000

    Added placeholders for possibly final functions for paper

---
+++ +otslm/+utils/find_roi.m
@@ -0,0 +1,5 @@
+function roi = find_roi(slm, cam, varargin)
+% FIND_ROI method to find region of camera SLM projects onto
+
+error('Not yet implemented');
+
---
+++ +otslm/+utils/image_device.m
@@ -0,0 +1,4 @@
+function im = image_device(slm, cam, varargin)
+% IMAGE_DEVICE method to generate image of slm
+
+error('Not yet implemented');
