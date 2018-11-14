afd9f55ca12966be683894ba65da9001b1c87c62
Isaac Lenton <isaac@isuniversal.com>
2018-11-14 18:50:25 +1000

    Added gui for mask region and sample region

--- gui/tools/Contents.m
+++ gui/tools/Contents.m
@@ -5,6 +5,9 @@
 %   Dither       - creates a binary patter from gray pattern
 %   Encode1d     - encode the target pattern amplitude into the phase
 %   Finalize     - finalize a pattern, applying a color map and taking the modulo
+%   MaskRegion   - adds patterns to base using masking
+%   SampleRegion - generates a pattern for sampling regions on SLM
+%   Visualise    - generates far-field plane images of the phase pattern
 %
 % Copyright 2018 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
