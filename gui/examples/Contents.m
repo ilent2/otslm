843efb52fa779438bfb48c820fd3aa5e173bbc65
Isaac Lenton <isaac@isuniversal.com>
2018-11-15 18:41:53 +1000

    Added an example

---
+++ gui/examples/Contents.m
@@ -0,0 +1,10 @@
+% OTSLM/GUI/EXAMPLES examples showing how to use user interface components
+%
+% Interfaces
+%   MixingTwoBeams    - demonstrating showing how to describe an incident
+%       Gaussian beam and split it into two spots using a mixture of
+%       two diffraction gratings.
+%
+% Copyright 2018 Isaac Lenton
+% This file is part of OTSLM, see LICENSE.md for information about
+% using/distributing this file.
