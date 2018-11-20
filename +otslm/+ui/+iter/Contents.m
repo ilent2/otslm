b1dae45b0fbd6ceead3de5e8720bfa802cf237a3
Isaac Lenton <isaac@isuniversal.com>
2018-11-20 15:55:29 +1000

    Implemented interface for iter/gs

--- +otslm/+ui/+iter/Contents.m
+++ +otslm/+ui/+iter/Contents.m
@@ -1,7 +1,11 @@
 % OTSLM/UI/ITER user interfaces for the iterative algorithms
 %
 % Interfaces
-%   gs    - Gerchberg-Saxton algorithm
+%   gs            - Gerchberg-Saxton and Adaptive-Adaptive algorithms
+%   direct_search - search through each pixel value to optimise hologram
+%   bsc           - optimisation in vector spherical wave function basis
+%   bowman2017    - Bowman 2017 conjugate gradient
+%   simulated_annealing - optimise the pattern using simulated annealing
 %
 % Copyright 2018 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
