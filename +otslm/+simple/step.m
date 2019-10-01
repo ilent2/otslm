03a4c0b02f3469ad0f4216a840aa53e5a22386ba
Isaac Lenton <isaac@isuniversal.com>
2019-10-01 12:25:06 +1000

    Simplified step function

--- +otslm/+simple/step.m
+++ +otslm/+simple/step.m
@@ -5,60 +5,30 @@ function pattern = step(sz, varargin)
 %
 % Optional named parameters:
 %
-%   'centre'    [ x, y ]    centre location for rotation (default: centre)
-%   'angle'     theta       angle in radians for gradient (from +x to +y)
-%   'angle_deg' theta       angle in degrees for gradient
 %   'value'     [ l, h ]    low and high values of step (default: [0, 0.5])
+%   'centre'      [x, y]      centre location for pattern
+%   'offset'      [x, y]      offset in rotated coordinate system
+%   'aspect'      aspect      aspect ratio of lens (default: 1.0)
+%   'angle'       angle       Rotation angle about axis (radians)
+%   'angle_deg'   angle       Rotation angle about axis (degrees)
+%   'gpuArray'    bool        If the result should be a gpuArray
 %
 % Copyright 2018 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
 % using/distributing this file.
 
 p = inputParser;
-p.addParameter('centre', [ sz(2)/2, sz(1)/2 ]);
-p.addParameter('angle', []);
-p.addParameter('angle_deg', []);
-p.addParameter('value', [0, 0.5]);
+p = addGridParameters(p, sz, 'skip', 'type');
+p.addParameter('value', [0.0, 0.5]);
 p.parse(varargin{:});
 
-% Generate grid
-[xx, yy] = meshgrid(1:sz(2), 1:sz(1));
-
-% Move centre of pattern
-xx = xx - p.Results.centre(1);
-yy = yy - p.Results.centre(2);
-
-% Apply rotation to pattern
-
-angle = [];
-if ~isempty(p.Results.angle)
-  assert(isempty(angle), 'Angle set multiple times');
-  angle = p.Results.angle;
-end
-if ~isempty(p.Results.angle_deg)
-  assert(isempty(angle), 'Angle set multiple times');
-  angle = p.Results.angle_deg * pi/180.0;
-end
-if isempty(angle)
-  angle = 0.0;
-end
-
-xxr = cos(angle).*xx - sin(angle).*yy;
-yyr = sin(angle).*xx + cos(angle).*yy;
-xx = xxr;
-yy = yyr;
+% Calculate coordinates
+gridParameters = expandGridParameters(p);
+[xx, ~] = otslm.simple.grid(sz, gridParameters{:});
 
 % Generate pattern
-
 pattern = xx >= 0;
 
-% Scale pattern
-
-low = p.Results.value(1);
-high = p.Results.value(2);
-
-pattern = pattern * (high - low) + low;
-
-% Ensure type of output matches low/high
-pattern = cast(pattern, 'like', p.Results.value);
+% Scale the pattern (convert from logical to double)
+pattern = castValue(pattern, p.Results.value);
 
