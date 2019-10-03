99f573242ecfc5730a178eeb48c91ce3eaf8bba2
Isaac Lenton <isaac@isuniversal.com>
2019-10-03 22:03:14 +1000

    Reduced code duplication in ui.iter package

---
+++ +otslm/+ui/+support/iterPatternValueChanged.m
@@ -0,0 +1,27 @@
+function iterPatternValueChanged(name, pattern, ...
+  device_name, enable_update, enable_display, ...
+  display_ax, display_type, display_name, fitness_method)
+% iterPatternValueChanged common code for iter update uis
+%
+% This should realy be part of the base class, but we don't seem
+% to be able to package apps with a custom base class.  Maybe in
+% future MATLAB versions this might be possible.
+%
+% Copyright 2019 Isaac Lenton
+% This file is part of OTSLM, see LICENSE.md for information about
+% using/distributing this file.
+
+% Save the pattern in the base workspace
+otslm.ui.support.saveVariableToBase(name, pattern);
+
+% Display image on the device
+slm = otslm.ui.support.getDeviceFromBase(device_name);
+if enable_update && ~isempty(slm)
+    slm.show(pattern./(2*pi));
+end
+
+% Generate display pattern and save to workspace
+if enable_display
+    otslm.ui.support.updateIterDisplay(pattern, slm, ...
+        display_type, display_ax, display_name, fitness_method)
+end
---
+++ +otslm/+ui/+support/updateIterDisplay.m
@@ -0,0 +1,45 @@
+function updateIterDisplay(pattern, slm, display_type, ax, ...
+    output_name, fitness_method)
+% updateSimpleDisplay helper for updating the display on simple uis
+%
+% updateSimpleDisplay(pattern, slm, display_type, ax, output_name)
+% Generates the display pattern, updates the axis and outputs to base.
+%
+% Copyright 2019 Isaac Lenton
+% This file is part of OTSLM, see LICENSE.md for information about
+% using/distributing this file.
+
+% Generate the pattern to display
+dpattern = [];
+switch display_type
+    case 'phase'
+        dpattern = pattern;
+    case 'error'
+        cla(ax, 'reset');
+        fitness_method();
+    case 'device'
+        if ~isempty(slm)
+            dpattern = slm.view(pattern./(2*pi));
+        else
+            warning('Device not setup');
+        end
+    case 'farfield'
+        dpattern = otslm.tools.visualise(pattern, 'method', 'fft', 'trim_padding', true, ...
+            'padding', ceil(size(pattern)/2), 'incident', ones(size(pattern)));
+        dpattern = abs(dpattern).^2;
+    otherwise
+        error('Invalid drop down option selected');
+end
+
+% Show the pattern in the window
+if ~isempty(dpattern)
+    imagesc(ax, dpattern);
+    axis(ax, 'image');
+    set(ax, 'xtick', [], 'xticklabel', []);
+    set(ax, 'ytick', [], 'yticklabel', []);
+end
+
+% Write pattern to workspace
+otslm.ui.support.saveVariableToBase(...
+    output_name, dpattern, 'Display variable');
+
