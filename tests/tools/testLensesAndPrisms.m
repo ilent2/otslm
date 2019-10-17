4de8183d8cfcd948f3e05fd913fdc11796558f8f
Isaac Lenton <isaac@isuniversal.com>
2019-10-17 22:07:21 +1000

    Added unit test for lensesandprisms

--- tests/tools/testLensesAndPrisms.m
+++ tests/tools/testLensesAndPrisms.m
@@ -16,3 +16,21 @@ function testSimple(testCase)
   pattern = otslm.tools.lensesAndPrisms(sz, xyz);
 
 end
+
+function testMatchCombineOutput(testCase)
+
+  sz = [100, 100];
+  cn = sz / 2;
+  xyz = [10, 5, 0.1; -3, -2, -0.2] ./ sz(1);
+  
+  pattern = otslm.tools.lensesAndPrisms(sz, xyz.');
+  
+  g1 = otslm.simple.linear(sz, 1./xyz(1, 1:2), 'centre', cn);
+  g2 = otslm.simple.linear(sz, 1./xyz(2, 1:2), 'centre', cn);
+  l1 = otslm.simple.parabolic(sz, xyz(1, 3), 'centre', cn);
+  l2 = otslm.simple.parabolic(sz, xyz(2, 3), 'centre', cn);
+  
+  target = otslm.tools.combine({mod(l1+g1, 1), mod(l2+g2, 1)}, 'method', 'super');
+  
+  testCase.verifyEqual(abs(0.5 - mod(pattern-target, 1)), 0.5*ones(sz), 'AbsTol', 0.02);
+end
