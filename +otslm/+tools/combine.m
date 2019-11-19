ilent2/otslm

commit ee4168250f7be180ce7d425a324957fdf6a17d34

Updateing docs for tools package to rst

+otslm/+tools/combine.m
@@ -1,36 +1,41 @@
 function pattern = combine(inputs, varargin)
-% COMBINE combines multiple patterns
-%
-% pattern = combine(inputs, ...) combines the cell array of patterns.
+% Combines multiple patterns
 %
 % Typical input should be a pattern between 0 and 1.
 % Most methods output range is between 0 and 1.
 %
-% See also Di Leonardo, Ianni and Ruocco (2007).
+% Usage
+%   pattern = combine(inputs, ...) combines the cell array of patterns.
 %
-% Optional named parameters:
+% Parameters
+%   - inputs (cell) -- cell array of input images to combine.
+%     These images should all be the same size.
 %
-%   'method'    method    Method to use when combinging patterns.
+% Optional named parameters
+%   - 'method' (enum) -- Method to use when combining patterns.
 %
-%       Methods to create multiple beams:
-%         dither        Randomly chooses values from different patterns
-%         super         Uses phi = angle(\sum_ii exp(1i*2*pi*inputs(ii)))
-%         rsuper        Superposition with random offset for each layer
-%         gs            Applies GS algorithm to try and generate the
-%             pattern that would be created by the sum of the individual
-%             beams.  Starts with an initial guess using rsuper.
+%       Methods to create multiple beams
+%         - dither   --   Randomly chooses values from different patterns
+%         - super    --   Uses phi = angle(\sum_ii exp(1i*2*pi*inputs(ii)))
+%         - rsuper   --   Superposition with random offset for each layer
+%         - gs       --   Applies GS algorithm to try and generate the
+%           pattern that would be created by the sum of the individual
+%           beams.  Starts with an initial guess using rsuper.
 %
-%       Methods to modulate a beam pattern:
-%         add           Adds the patterns: \sum_ii inputs(ii)
-%         multiply      Multiplies the patterns: \prod_ii inputs(ii)
-%         addangle      Uses phi = angle(\prod_ii exp(1i*2*pi*inputs(ii)))
-%         average       Weighted average of inputs.  (default weights: ones)
-%                           \sum_ii w_ii*inputs(ii) / \sum_ii w_ii
+%       Methods to modulate a beam pattern
+%         - add      --   Adds the patterns: \sum_ii inputs(ii)
+%         - multiply --   Multiplies the patterns: \prod_ii inputs(ii)
+%         - addangle --   Uses phi = angle(\prod_ii exp(1i*2*pi*inputs(ii)))
+%         - average  --   Weighted average of inputs.  (default weights: ones)
+%           :math:`\sum_i w_i I_i / \sum_i w_i`
 %
 %       Default method: super.
 %
-%   'weights'   [weights] Array of weights for each pattern.
+%   - 'weights' (numeric) -- Array of weights, one for each pattern.
+%     (default: [], uses equal weights for each pattern)
 %
+% See also Di Leonardo, Ianni and Ruocco (2007).
+
 % Copyright 2018 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
 % using/distributing this file.
@@ -48,7 +53,7 @@ weights = p.Results.weights;
 if isempty(weights)
   weights = ones(length(inputs), 1);
 end
-nweights = weights ./ sum(weights);
+nweights = ones(size(weights)); %weights ./ sum(weights);
 
 
 switch p.Results.method

+otslm/+tools/bsc2hologram.m
@@ -1,17 +1,25 @@
 function [pattern, cpattern] = bsc2hologram(sz, beam, varargin)
-% BSC2HOLOGRAM calculates the far-field hologram for a BSC beam
+% Calculates the far-field hologram for a BSC beam
 %
-% [phase, cpattern] = bsc2hologram(sz, beam, ...) calculates the phase
-% pattern that transforms the incident beam to the BSC beam.
-% Additionally, outputs the complex x and y polarisation complex
-% amplitudes of the BSC beam in the far-field (szx2 matrix).
+% .. warning:: The current version of the optical tweezers toolbox
+%    may introduce additional phase artifacts in the far-field.
 %
-% Optional named parameters:
-%   'incident'      im      Incident beam complex amplitude (default: ones)
-%   'polarisation'  [x y]   Polarisation of incident beam (default: [1 1i])
-%   'encodemethod'  str     Amplitude encode method (see tools.finalize)
-%   'radius'        r       Radius of the hologram pattern (default: 1.0)
+% Usage
+%   [phase, cpattern] = bsc2hologram(sz, beam, ...) calculates the phase
+%   pattern that transforms the incident beam to the BSC beam.
+%   Additionally, outputs the complex x and y polarisation complex
+%   amplitudes of the BSC beam in the far-field (szx2 matrix).
 %
+% Parameters
+%   - sz -- size of pattern
+%   - beam -- Optical tweezers toolbox Bsc beam object
+%
+% Optional named parameters
+%   - 'incident'      im    -- Incident beam complex amplitude (default: ones)
+%   - 'polarisation'  [x y] -- Polarisation of incident beam (default: [1 1i])
+%   - 'encodemethod'  str   -- Amplitude encode method (see tools.finalize)
+%   - 'radius'        r     -- Radius of the hologram pattern (default: 1.0)
+
 % Copyright 2018 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
 % using/distributing this file.

+otslm/+tools/castValue.m
@@ -1,10 +1,16 @@
 function pattern = castValue(pattern, value)
-% castValue convert from logical pattern to specified value range
+% Convert from logical pattern to specified value range
 %
-% pattern = castValue(pattern, [min, max]) converts from a logical
-% pattern to the specified value range.  If [min, max] isan empty
-% array, leaves the pattern as a logical array.
+% Usage
+%   pattern = castValue(pattern, value)
 %
+% Parameters
+%   - pattern (logical) -- the pattern to be converted
+%   - value (numeric) -- values for logical false and logical true
+%     pattern values.  Should be either a 2 element vector
+%     ``[false_value, true_value]``.or an empty array to leave the
+%     values as logical.
+
 % Copyright 2019 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
 % using/distributing this file.

+otslm/+tools/colormap.m
@@ -1,15 +1,37 @@
 function pattern = colormap(pattern, cmap, varargin)
-% COLORMAP applies a colormap to a pattern
+% Applies a colormap to a pattern.
 %
-% pattern = colormap(pattern, colormap, ...) applies the colormap to
-% the pattern.  The input pattern should have a typical range from 0 to 1.
-% If the colormap is a LookupTable, the input pattern is scaled by the
-% lookup table range.
+% This method either applies nearest value interpolation or uses a
+% predefined lookup table.
 %
-% Optional named parameters:
-%   'inverse'   bool    Apply inverse colormap.  The output will have
-%       a typical range from 0 to 1.
+% If a discrete colormap is provided, only values present in the
+% colormap are used for the output pattern, allowing the colormap to
+% contain discrete device specific values.
 %
+% Usage
+%   pattern = colormap(pattern, colormap, ...) applies the colormap to
+%   the pattern.  The input pattern should have a typical range from 0 to 1.
+%   If the colormap is a LookupTable, the input pattern is scaled by the
+%   lookup table range.
+%
+% Parameters
+%   - pattern (numeric) -- the pattern to be converted
+%   - colormap (LookupTable|numeric|cell|enum) -- colormap to apply.
+%     The way colormaps are applied depends on the colormap type:
+%     - LookupTable -- Uses phase, value and range properties of
+%       the :class:`utils.LookupTable` object.
+%     - numeric -- assumes colormap is a vector of equally spaced values
+%       for the phase corresponding to pattern values between 0 and 1.
+%     - cell -- assumes colormap is a 2 element cell array.  The first
+%       element is a vector with pattern values (range 0 to 1) and the
+%       second column is the corresponding output values.
+%     - enum -- 'pmpi', '2pi', 'bin' or 'gray' for output range between
+%       plus/minus pi, 0 to 2pi, binary, or grayscale (unchanged).
+%
+% Optional named parameters
+%   - 'inverse' (logical) -- Apply inverse colormap.  The output will have
+%       a typical range from 0 to 1.  Not implemented for all colormap types.
+
 % Copyright 2018 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
 % using/distributing this file.

+otslm/+tools/dither.m
@@ -1,21 +1,25 @@
 function pattern = dither(pattern, level, varargin)
-% DITHER creates a binary patter from gray pattern
+% Creates a binary pattern from gray-scale image.
+% Supports several different dithering methods.
 %
-% pattern = dither(pattern, level, ...) applies the default dithering,
-% binary threshold, to the pattern.
+% Usage
+%   pattern = dither(pattern, level, ...) applies the default dithering,
+%   binary threshold, to the pattern.
 %
-% Most methods assume the pattern is in the range 0 to 1.
+% Parameters
+%   - pattern (numeric) -- the gray-scale pattern.  Most methods
+%     assume the pattern has values in the range 0 to 1.
+%   - level (numeric) -- threshold level
 %
-% Optional named parameters:
-%     'method'    method      Method to use for dithering
-%         Supported methods:
-%             'threshold'     Apply threshold filter to image (default)
-%             'mdither'       Use matlab dither function
-%             'floyd'         Floyd-Steinberg algorithm
-%             'random'        Does random dithering
-%     'value'    [min, max]   Value range for output image
-%         (default: [] for logical images)
-%
+% Optional named parameters
+%   - 'method' (enum) -- Method to use for dithering. Supported methods:
+%    - 'threshold' --  Apply threshold filter to image (default)
+%    - 'mdither'   --  Use matlab dither function
+%    - 'floyd'     --  Floyd-Steinberg algorithm
+%    - 'random'    --  Does random dithering
+%   - 'value'    [min, max]   Value range for output image
+%     (default: [] for logical images).  See :func:`castValue`.
+
 % Copyright 2018 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
 % using/distributing this file.

+otslm/+tools/encode1d.m
@@ -1,15 +1,21 @@
 function [pattern, assigned] = encode1d(target, varargin)
 % Encode the target pattern amplitude into the phase pattern size
 %
-% [pattern, assigned] = encode1d(target, ...) encodes the complex
-% target pattern into a 1-D phase mask.
+% Usage
+%   [pattern, assigned] = encode1d(target, ...) encodes the complex
+%   target pattern into a 1-D phase mask.
+%   Returns the encoded pattern and a logical matrix of the same size
+%   that specifies if pixels were used to encode the pattern.
 %
-% Optional named arguments:
+% Parameters
+%   - target (numeric) -- vector containing values of 1-D amplitude function
+%     to be encoded.
 %
-%   'scale'       scale       Scale for the height of the pattern
-%   'angle'       angle       Rotation angle about axis (radians)
-%   'angle_deg'   angle       Rotation angle about axis (degrees)
-%
+% Optional named arguments
+%  - 'scale' (numeric)     -- Scale for the height of the pattern
+%  - 'angle' (numeric)     -- Rotation angle about axis (radians)
+%  - 'angle_deg' (numeric) --  Rotation angle about axis (degrees)
+
 % Copyright 2018 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
 % using/distributing this file.

+otslm/+tools/finalize.m
@@ -1,6 +1,7 @@
 function pattern = finalize(pattern, varargin)
-% FINALIZE finalize a pattern, applying a color map and taking the modulo.
+% Finalize a pattern, applying a color map and taking the modulo.
 %
+% Usage
 %   pattern = finalize(input, ...) finalizes the pattern.
 %   For dmd type devices, the input is assumed to be the amplitude.
 %   For slm type devices, the input is assumed to be the phase.
@@ -8,35 +9,36 @@ function pattern = finalize(pattern, varargin)
 %   pattern = finalize(phase, 'amplitude', amplitude', ...) attempts
 %   to generate a pattern encoding both the phase and amplitude.
 %
-% Optional named parameters:
+% Parameters
+%   - pattern (numeric) -- phase pattern to be finalized
 %
-%   'modulo'    mod     Applies modulo to the pattern, default 1.0 for slm.
-%       Use 'none' for no modulo (default dmd).
+% Optional named parameters
+%   - 'modulo' (numeric|enum) -- Applies modulo to the pattern.
+%     Modulo should either be a scalar or 'none' for no modulo.
+%     (default: 1.0 for slm type devices and 'none' for dmd type devices).
 %
-%   'colormap'  lookup  Applies the nearest value colour map lookup.
-%       May also be:
-%         'pmpi'  for -pi to pi range (default for slm)
-%         '2pi'   for 0 to 2*pi range
-%         'bin'   for binary amplitude
-%         'gray'  for 0 to 1 range (default for dmd)
+%   - 'colormap' -- Colormap to apply to pattern.  For a list of
+%     valid values, see :func:`colormap`.
+%     (default: 'pmpi' for slm and 'gray' for dmd type devices).
 %
-%   'rpack'     type    Rotation packing of the pixels
-%       Supported types:
-%         'none'  No additional steps required (default slm)
-%         '45deg' Device is rotated 45 degrees (aspect 1:2, default dmd)
+%   - 'rpack' (enum) -- rotation packing of the pixels.
+%    - 'none'  -- No additional steps required (default slm)
+%    - '45deg' -- Device is rotated 45 degrees (aspect 1:2, default dmd)
 %
-%   'device'    type    Specifies the type of device.
-%       Supported devices:
-%         'dmd'   Digital micro mirror (amplitude) device
-%         'slm'   Spatial light modulator (phase) device
+%   - 'device' (enum) -- Specifies the type of device, changes the
+%     default value for most arguments.  If all arguments are provided,
+%     this argument has no impact.
+%    - 'dmd' -- Digital micro mirror (amplitude) device
+%    - 'slm' -- Spatial light modulator (phase) device
 %
-%   'encodemethod' method Method to use when encoding phase/amplitude
-%       'checker'   Use checkerboard pattern and acos correction (phase)
-%       'grating'   Use linear grating and sinc correction (phase)
-%       'magnitude' Use grating magnitude modulation (phase)
+%   - 'encodemethod' method Method to use when encoding phase/amplitude
+%    - 'checker'   -- (default) use checkerboard pattern and acos
+%      correction (phase)
+%    - 'grating'   -- Use linear grating and sinc correction (phase)
+%    - 'magnitude' -- Use grating magnitude modulation (phase)
 %
-%   'amplitude' pattern Amplitude pattern to generate output for
-%
+%   - 'amplitude' pattern Amplitude pattern to generate output for
+
 % Copyright 2018 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
 % using/distributing this file.

+otslm/+tools/hologram2bsc.m
@@ -1,24 +1,32 @@
 function beam = hologram2bsc(pattern, varargin)
-% HOLOGRAM2BSC convert pattern to beam shape coefficients
+% Convert 2-D paraxial pattern to beam shape coefficients
 %
-% beam = hologram2bsc(pattern, ...) converts the pattern to a BSC beam.
-% If pattern is real, assumes a phase pattern, else assumes complex amplitude.
+% This function uses the Optical Tweezers Toolbox BscPmParaxial class
+% to calculate the beam shape coefficients using point matching.
 %
-% Optional named parameters:
-%   incident   pattern    Uses the incident illumination
-%   amplitude  amplitude  Specifies the amplitude of the pattern
-%   Nmax       num        The VSWF truncation number
-%   polarisation [x,y]    Polarisation of the VSWF beam
-%   index_medium num      Refractive index of medium
-%   NA           num      Numerical aperture of objective
-%   wavelength0  num      Wavelength of light in vacuum (default: 1)
-%   omega        num      Angular frequency of light (default: 2*pi)
-%   beamData     beam     Pass an existing Paraxial beam to reuse
-%     the pre-computed special functions.
-%   keep_coefficient_matrix  bool  Calculate the inverse coefficient
+% Usage
+%   beam = hologram2bsc(pattern, ...) converts the pattern to a BSC beam.
+%   If pattern is real, assumes a phase pattern, else assumes complex amplitude.
+%
+% Parameters
+%   pattern (numeric) -- the pattern to convert
+%
+% Optional named parameters
+%   - incident (numeric)  -- Uses the incident illumination
+%   - amplitude (numeric) -- Specifies the amplitude of the pattern
+%   - Nmax       num      -- The VSWF truncation number
+%   - polarisation [x,y]  -- Polarisation of the VSWF beam
+%   - index_medium num    -- Refractive index of medium
+%   - NA           num    -- Numerical aperture of objective
+%   - wavelength0  num    -- Wavelength of light in vacuum (default: 1)
+%   - omega        num    -- Angular frequency of light (default: 2*pi)
+%   - beamData     beam   -- Pass an existing Paraxial beam to reuse
+%     the pre-computed special functions.  This requires the previous
+%     beam to have been generated with the keep_coefficient_matrix option.
+%   - keep_coefficient_matrix (logical) -- Calculate the inverse coefficient
 %     matrix and store it with the beam.  This is slower for a single
 %     calculation but can be faster for repeated calculation. Default: false.
-%
+
 % Copyright 2018 Isaac Lenton
 % This file is part of OTSLM, see LICENSE.md for information about
 % using/distributing this file.

commit 92993e2924eda65e25f2a56a2153dc8b198c5838

Bugfix: weights in tools.combine were not being normalised correctly
  This fixes an issue introduced in ee41682 where the weights were
  being set to ones (not sure why).  This commit should have been
  changes only to the documentation.

+otslm/+tools/combine.m
@@ -66,7 +66,7 @@ weights = p.Results.weights;
 if isempty(weights)
   weights = ones(length(inputs), 1);
 end
-nweights = ones(size(weights)); %weights ./ sum(weights);
+nweights = weights ./ sum(weights);
 
 
 switch p.Results.method